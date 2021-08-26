fe7c92588b2aab8cb1a2dee7e1aee9cc525629b4
Shahin-Mahmud98 <shahinmahmud412@gmail.com>
2021-08-26 06:27:38 +0600

    file via basic

---
+++ basic-practice.js
@@ -0,0 +1,33 @@
+var deposit
+
+// array
+const numbers = [27, 29, 93, 034, 8];
+const numberCount = numbers.length;
+numbers.push(111);
+numbers.pop();
+// numbers[2]=545; (this is number chnaging condition for array and push the array)
+
+// check wheather 222 included in the array 
+if (numbers.indexOf(222) != -1) {
+
+}
+if (numbers.includes(222)) {
+
+}
+
+// loop
+// while ,for
+// for holo kono ekta element k mane kono ekta element er number of number ber korar jonno kaj kore 
+
+for (const number of numbers) {
+
+}
+
+// function
+function fullName(first, seecond) {
+    const name = first + ' ' + seecond;
+}
+const person = fullName('Rahim', 'Ali');
+
+// object holo nijer soriler kotha vaben ba nijer life style er kotha vaben jemon dhorun amar nak kan chok mukh its an object
+
---
+++ let-const.js
@@ -0,0 +1,18 @@
+// error
+// Const balance = 1240;
+// balance = 1340;
+
+let balance = 1240;
+balance = 1340;
+
+
+const userName = 'jan Pakhi potas'
+const weTogether = 'ami' + userName;
+console.log(weTogether);
+// error
+// userName = 'moyna '
+
+const numbers = [12, 36, 37, 25];
+// numbers = [53,364]; not allowed
+
+
